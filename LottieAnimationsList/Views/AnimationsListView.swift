56a77e1ddbc172b3843c23019bd008c166e5d3f5
Dawn Liu <dliu@lcs.on.ca>
2024-01-30 14:36:35 -0500

    Added Mr. Gordon's helper view.

---
+++ LottieAnimationsList/Views/AnimationsListView.swift
@@ -0,0 +1,18 @@
+//
+//  AnimationsListView.swift
+//  LottieAnimationsList
+//
+//  Created by Chenxi Liu on 2024/1/30.
+//
+
+import SwiftUI
+
+struct AnimationsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimationsListView()
+}
