f8edd649aeffd40d46415f7b51d5c7dd1256dcf0
Dawn Liu <dliu@lcs.on.ca>
2024-01-30 14:23:32 -0500

    Initial Commit

---
+++ LottieAnimationsList/LottieAnimationsListApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieAnimationsListApp.swift
+//  LottieAnimationsList
+//
+//  Created by Chenxi Liu on 2024/1/30.
+//
+
+import SwiftUI
+
+@main
+struct LottieAnimationsListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
